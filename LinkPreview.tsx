f548ec14899d23a8f2c8901aef44211f1f56e137
Dinh Thanh Huyen <71447270+dtthuyen@users.noreply.github.com>
2024-04-03 15:25:30 +0700

    Update LinkPreview.tsx
    update regex link

--- LinkPreview.tsx
+++ LinkPreview.tsx
@@ -1,213 +1,232 @@
-import * as React from 'react'
-import {
-  Image,
-  LayoutAnimation,
-  LayoutChangeEvent,
-  Linking,
-  StyleProp,
-  StyleSheet,
-  Text,
-  TouchableWithoutFeedback,
-  TouchableWithoutFeedbackProps,
-  View,
-  ViewStyle,
-} from 'react-native'
+import { decode } from 'html-entities'
+import { Image } from 'react-native'
 
-import { PreviewData, PreviewDataImage } from './types'
-import { getPreviewData, oneOf } from './utils'
-import styled from 'styled-components/native'
-import { Fonts } from '@base/ui-kit'
+import { PreviewData, PreviewDataImage, Size } from './types'
+import { getPlainText } from '@base/core'
 
-export interface LinkPreviewProps {
-  containerStyle?: StyleProp<ViewStyle>
-  enableAnimation?: boolean
-  metadataContainerStyle?: StyleProp<ViewStyle>
-  metadataTextContainerStyle?: StyleProp<ViewStyle>
-  onPreviewDataFetched?: (previewData: PreviewData) => void
-  previewData?: PreviewData
-  renderDescription?: (description: string) => React.ReactNode
-  renderImage?: (image: PreviewDataImage) => React.ReactNode
-  renderLinkPreview?: (payload: {
-    aspectRatio?: number
-    containerWidth: number
-    previewData?: PreviewData
-  }) => React.ReactNode
-  renderTitle?: (title: string) => React.ReactNode
-  requestTimeout?: number
-  text: string
-  touchableWithoutFeedbackProps?: TouchableWithoutFeedbackProps
+export const getActualImageUrl = (baseUrl: string, imageUrl?: string) => {
+  let actualImageUrl = imageUrl?.trim()
+  if (!actualImageUrl || actualImageUrl.startsWith('data')) return
+
+  if (actualImageUrl.startsWith('//'))
+    actualImageUrl = `https:${actualImageUrl}`
+
+  if (!actualImageUrl.startsWith('http')) {
+    if (baseUrl.endsWith('/') && actualImageUrl.startsWith('/')) {
+      actualImageUrl = `${baseUrl.slice(0, -1)}${actualImageUrl}`
+    } else if (!baseUrl.endsWith('/') && !actualImageUrl.startsWith('/')) {
+      actualImageUrl = `${baseUrl}/${actualImageUrl}`
+    } else {
+      actualImageUrl = `${baseUrl}${actualImageUrl}`
+    }
+  }
+
+  return actualImageUrl
 }
 
-const containerWidth = 60;
+export const getHtmlEntitiesDecodedText = (text?: string) => {
+  const actualText = text?.trim()
+  if (!actualText) return
 
-export const LinkPreview = React.memo(
-  ({
-    containerStyle,
-    enableAnimation,
-    metadataContainerStyle,
-    metadataTextContainerStyle,
-    onPreviewDataFetched,
-    previewData,
-    renderDescription,
-    renderImage,
-    renderLinkPreview,
-    renderTitle,
-    requestTimeout = 5000,
-    text,
-    touchableWithoutFeedbackProps,
-  }: LinkPreviewProps) => {
-    const [data, setData] = React.useState(previewData)
-    const aspectRatio = data?.image
-      ? data.image.width / data.image.height
-      : undefined
+  return decode(actualText)
+}
 
-    React.useEffect(() => {
-      let isCancelled = false
-      if (previewData) {
-        setData(previewData)
-        return
-      }
-
-      const fetchData = async () => {
-        setData(undefined)
-        const newData = await getPreviewData(text, requestTimeout)
-        // Set data only if component is still mounted
-        /* istanbul ignore next */
-        if (!isCancelled) {
-          // No need to cover LayoutAnimation
-          /* istanbul ignore next */
-          if (enableAnimation) {
-            LayoutAnimation.easeInEaseOut()
-          }
-          setData(newData)
-          onPreviewDataFetched?.(newData)
-        }
-      }
-
-      fetchData()
-      return () => {
-        isCancelled = true
-      }
-    }, [
-      enableAnimation,
-      onPreviewDataFetched,
-      previewData,
-      requestTimeout,
-      text,
-    ])
-
-    const handlePress = () => data?.link && Linking.openURL(data.link)
-
-    const renderTitleNode = (title: string) => {
-      return oneOf(
-        renderTitle,
-        <STitle numberOfLines={2} >
-          {title}
-        </STitle>
-      )(title)
-    }
-
-    const renderDescriptionNode = (description: string) => {
-      return oneOf(
-        renderDescription,
-        <SText numberOfLines={3} >
-          {description}
-        </SText>
-      )(description)
-    }
-
-    const renderImageNode = (image: PreviewDataImage) => {
-      const ar = aspectRatio ?? 1
-
-      return oneOf(
-        renderImage,
-        <Image
-          accessibilityRole='image'
-          resizeMode='contain'
-          source={{ uri: image.url }}
-          style={StyleSheet.flatten([
-            { marginRight: 8, borderRadius: 2 },
-            ar < 1
-              ? {
-                height: containerWidth,
-                width: containerWidth * ar,
-              }
-              : {
-                height: containerWidth / ar,
-                width: containerWidth,
-              }
-          ])}
-        />
-      )(image)
-    }
-
-    const renderLinkPreviewNode = () => {
-      return oneOf(
-        renderLinkPreview,
-        <ViewPreviewLink style={containerStyle}>
-          <View
-              style={StyleSheet.flatten([
-                styles.metadataContainer,
-                metadataContainerStyle,
-              ])}
-            >
-              {data?.image && renderImageNode(data.image)}
-              <View
-                style={StyleSheet.flatten([
-                  styles.metadataTextContainer,
-                  metadataTextContainerStyle,
-                ])}
-              >
-                {data?.title && renderTitleNode(data.title)}
-                {data?.description && renderDescriptionNode(data.description)}
-              </View>
-            </View>
-        </ViewPreviewLink>
-      )({
-        aspectRatio,
-        containerWidth,
-        previewData: data,
-      })
-    }
-
-    return ((data?.description || data?.title) ? (
-      <TouchableWithoutFeedback
-        accessibilityRole='button'
-        onPress={handlePress}
-        {...touchableWithoutFeedbackProps}
-      >
-        {renderLinkPreviewNode()}
-      </TouchableWithoutFeedback>
-    ) : <></>
-    )
+export const getContent = (left: string, right: string, type: string) => {
+  const contents = {
+    [left.trim()]: right,
+    [right.trim()]: left,
   }
-)
 
-const ViewPreviewLink = styled.View`
-  background-color: ${p => p.theme.grey6};
-  border-bottom-width: 0.5px;
-  border-top-width: 0.5px;
-  border-color: ${p => p.theme.grey5};
-`
+  return contents[type]?.trim()
+}
 
-const STitle = styled.Text`
-  font-size: 13px;
-  font-family: ${Fonts.Bold};
-  color: ${p => p.theme.grey1};
-`
+export const getImageSize = (url: string) => {
+  return new Promise<Size>((resolve, reject) => {
+    Image.getSize(
+      url,
+      (width, height) => {
+        resolve({ height, width })
+      },
+      // type-coverage:ignore-next-line
+      (error) => reject(error)
+    )
+  })
+}
 
-const SText = styled.Text`
-  font-size: 12px;
-  color: ${p => p.theme.grey2};
-  margin-top: 4px;
-`
+// Functions below use functions from the same file and mocks are not working
+/* istanbul ignore next */
+export const getPreviewData = async (text: string, requestTimeout = 5000) => {
+  const previewData: PreviewData = {
+    description: undefined,
+    image: undefined,
+    link: undefined,
+    title: undefined,
+    domain: undefined
+  }
 
-const styles = StyleSheet.create({
-  metadataContainer: {
-    flexDirection: 'row',
-    padding: 12
-  },
-  metadataTextContainer: {
-    flex: 1,
-  },
-})
+  try {
+    const textWithoutEmails = getPlainText(text).replace(REGEX_EMAIL, '').trim()
+
+    if (!textWithoutEmails) return previewData
+    
+    const link = textWithoutEmails.match(REGEX_LINK)?.[0]
+    
+    try {
+      const _url = require("url").parse(link, true);
+      if(_url.hostname) previewData.domain = _url.hostname;
+    } catch (e) {}
+    
+    if (!link) return previewData
+    
+    let url = link
+    
+    if (!url.toLowerCase().startsWith('http')) {
+      url = 'https://' + url
+    }
+
+    // eslint-disable-next-line no-undef
+    let abortControllerTimeout: NodeJS.Timeout
+    const abortController = new AbortController()
+
+    const request = fetch(url, {
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+      },
+      signal: abortController.signal,
+    })
+
+    abortControllerTimeout = setTimeout(() => {
+      abortController.abort()
+    }, requestTimeout)
+
+    const response = await request
+
+    clearTimeout(abortControllerTimeout)
+
+    previewData.link = url
+
+    const contentType = response.headers.get('content-type') ?? ''
+
+    if (REGEX_IMAGE_CONTENT_TYPE.test(contentType)) {
+      const image = await getPreviewDataImage(url)
+      previewData.image = image
+      return previewData
+    }
+
+    const html = await response.text()
+
+    // Some pages return undefined
+    if (!html) return previewData
+    
+    const head = html.substring(0, html.indexOf('<body'))
+    // Get page title
+    // const title = head.match(REGEX_TITLE)
+    const title = REGEX_TITLE.exec(head)
+    previewData.title = getHtmlEntitiesDecodedText(title?.[1])
+
+    let matches: RegExpMatchArray | null
+    const meta: RegExpMatchArray[] = []
+    while ((matches = REGEX_META.exec(head)) !== null) {
+      meta.push([...matches])
+    }
+
+    const metaPreviewData = meta.reduce<{
+      description?: string
+      imageUrl?: string
+      title?: string
+    }>(
+      (acc, curr) => {
+        // Verify that we have property/name and content
+        // Note that if a page will specify property, name and content in the same meta, regex will fail
+        if (!curr[2] || !curr[3]) return acc
+
+        // Only take the first occurrence
+        // For description take the meta description tag into consideration
+        const description =
+          !acc.description &&
+          (getContent(curr[2], curr[3], 'og:description') ||
+            getContent(curr[2], curr[3], 'description'))
+        const ogImage =
+          !acc.imageUrl && getContent(curr[2], curr[3], 'og:image')
+        const ogTitle = !acc.title && getContent(curr[2], curr[3], 'og:title')
+
+        return {
+          description: description
+            ? getHtmlEntitiesDecodedText(description)
+            : acc.description,
+          imageUrl: ogImage ? getActualImageUrl(url, ogImage) : acc.imageUrl,
+          title: ogTitle ? getHtmlEntitiesDecodedText(ogTitle) : acc.title,
+        }
+      },
+      { title: previewData.title }
+    )
+    
+    previewData.description = metaPreviewData.description
+    previewData.image = await getPreviewDataImage(metaPreviewData.imageUrl)
+    previewData.title = metaPreviewData.title
+
+    if (!previewData.image) {
+      let imageMatches: RegExpMatchArray | null
+      const tags: RegExpMatchArray[] = []
+      while ((imageMatches = REGEX_IMAGE_TAG.exec(html)) !== null) {
+        tags.push([...imageMatches])
+      }
+
+      let images: PreviewDataImage[] = []
+
+      for (const tag of tags
+        .filter((t) => !t[1].startsWith('data'))
+        .slice(0, 5)) {
+        const image = await getPreviewDataImage(getActualImageUrl(url, tag[1]))
+
+        if (!image) continue
+
+        images = [...images, image]
+      }
+
+      previewData.image = images.sort(
+        (a, b) => b.height * b.width - a.height * a.width
+      )[0]
+    }
+
+    return previewData
+  } catch {
+    return previewData
+  }
+}
+
+/* istanbul ignore next */
+export const getPreviewDataImage = async (url?: string) => {
+  if (!url) return
+
+  try {
+    const { height, width } = await getImageSize(url)
+    const aspectRatio = width / (height || 1)
+
+    if (height > 100 && width > 100 && aspectRatio > 0.1 && aspectRatio < 10) {
+      const image: PreviewDataImage = { height, url, width }
+      return image
+    }
+  } catch {}
+}
+
+export const oneOf =
+  <T extends (...args: A) => any, U, A extends any[]>(
+    truthy: T | undefined,
+    falsy: U
+  ) =>
+  (...args: Parameters<T>): ReturnType<T> | U => {
+    return truthy ? truthy(...args) : falsy
+  }
+
+export const REGEX_EMAIL = /([a-zA-Z0-9+._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/g
+export const REGEX_IMAGE_CONTENT_TYPE = /image\/*/g
+// Consider empty line after img tag and take only the src field, space before to not match data-src for example
+export const REGEX_IMAGE_TAG = /<img[\n\r]*.*? src=["'](.*?)["']/g
+export const REGEX_LINK =/((http|https|ws|wss):\/\/)?[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z]{2,}(\/[^\s]*)?/gi
+// /((http|ftp|https|ws|wss):\/\/)?([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?/i
+// Some pages write content before the name/property, some use single quotes instead of double
+export const REGEX_META =
+  /<meta.*?(property|name)=["'](.*?)["'].*?content=["'](.*?)["'].*?>/g
+export const REGEX_TITLE = /<title.*?>(.*?)<\/title>/g
